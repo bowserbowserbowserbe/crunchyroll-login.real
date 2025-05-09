db3b189ff2ad66ab5e418870c5312d50b354de02
bowserbowserbowserbe <159852473+bowserbowserbowserbe@users.noreply.github.com>
2025-05-09 22:01:27 +0200

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from crunchyroll import Crunchyroll
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # puoi restringere questo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class LoginRequest(BaseModel):
     email: str
